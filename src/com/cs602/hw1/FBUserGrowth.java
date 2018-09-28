488c623b73e2989dfc5dd72e95b800e6b1a318da
Nan Li <nanli@Nans-MacBook-Pro.local>
2018-09-27 23:49:39 -0400

    fb

---
+++ src/com/cs602/hw1/FBUserGrowth.java
@@ -0,0 +1,21 @@
+package com.cs602.hw1;
+
+public class FBUserGrowth {
+
+    public static int getMonth(double principal, double rate, double amount){
+        int n = (int)(Math.log(amount/principal)/Math.log(1.0 + rate));
+        return n;
+    }
+
+    public static void main (String [] args) {
+
+        System.out.printf("\nUser base will be 1,500,000,000 in these months:\n");
+        System.out.println(getMonth(1000000000.0, 0.04, 1500000000.0));
+
+        System.out.printf("\nUser base will be 2,000,000,000 in these months:\n");
+        System.out.println(getMonth(1000000000.0, 0.04, 2000000000.0));
+
+    }
+}
+
+

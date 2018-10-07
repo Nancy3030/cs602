877ba6d6480805b24f6d25abf7a8c395caa01e1f
Nan Li <nanli@Nans-MacBook-Pro.local>
2018-10-06 22:09:51 -0400

    Knight_Tour English

--- src/com/cs602/hw1/Knight_Tour.java
+++ src/com/cs602/hw1/Knight_Tour.java
@@ -1,17 +1,18 @@
-package Knight_Tour;
+package com.cs602.hw1;
 
 import java.util.Scanner;
 
 public class Knight_Tour {
-	//输入行走路线和棋盘大小
+	//set eight possible moves(direction) of the knight
 	static int X_Direction[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-	static int Y_Direction[] = {1, 2, 2, 1, -1, -2, -2, -1};
+	static int Y_Direction[] = {1, 2, 2, 1, -1, -2, -2, -1};  // set the size for the chase board
 	static int n = 8;
-	//输入点（x,y）是否在棋盘内并且未被走过
+	
+	//To determine to which step did not be occupied yet  
     static boolean Valid(int X, int Y, int board[][]) { 
         return (X >= 0 && X < n && Y >= 0 && Y < n && board[X][Y] == 0);
     }
-    //输入点（x,y）的下一步能走的位置数
+    //calculate how many possible for "the input" (X, Y)  
     static int GetDirections(int X, int Y, int board[][]) {
 		int Directions = 0;
 		for (int i = 0; i < 8; i++) {
@@ -21,35 +22,35 @@ public class Knight_Tour {
 		}
 		return Directions;
 	}
-    //从第二步开始进行递归
+    //Using the recursive from the second step
     static boolean Recursion (int X, int Y, int board[][]) {
     	int NextstepX = X; 
 		int NextstepY = Y;
-		//计算从第二步开始，每一步的落点
+		// calculate how many possible for every step 
     	for (int StepNum = 2; StepNum <= n * n; StepNum ++) {
-    		int choices = GetDirections(X, Y, board);	//计算点（x,y）的下一步能走的位置数
+    		int choices = GetDirections(X, Y, board);	//calculate how many possibility for every (X, Y)
     		if (choices == 0) {
     			return false;
     		}  else {
-    			int MinRoute = 8;	 //由于对每一个点来说最多只可能有8个方向能走，在计算最少的路径时将初始值赋值为8较为合理
-        		int Routes[] = new int [choices];	//由上面的计算可知下一步能走的位置数，所以需要对每一个位置计算其下一步能走的位置数
-        		//计算点（x，y）的所有下一步能走的位置中出路最少的一个点
+    			int MinRoute = 8;	 // There are only eight possibilities for every step. So, we initialize the value for 8 and whenever a step is occupied, we minus 1  
+        		int Routes[] = new int [choices];	// calculate how many possible for eight step and put into the array
+        		// calculate which direction has the fewest pass possibility
         		for (int i = 0; i < choices; i++) {
         			for (int j = 0; j < 8; j++) {
         				int NextX = X + X_Direction[j];
             			int NextY = Y + Y_Direction[j];
-            			Routes[i] = GetDirections(NextX, NextY, board);		//计算每一个点（NextX,NextY）的下一步能走的位置数
+            			Routes[i] = GetDirections(NextX, NextY, board);		//calculate how many possible for the (NextX,NextY)
             			if (Valid(NextX, NextY, board) && Routes[i] < MinRoute) {
             				MinRoute = Routes[i];
             				NextstepX = NextX;
-            				NextstepY = NextY;	//将下一步能走的位置数最少的其中一个点（NextX,NextY）的坐标赋值给NextstepX，NextstepY
+            				NextstepY = NextY;	// calculate which direction has fewest possibility and put into the (NextX,NextY)
             			}
         			}
         		}
             
             X = NextstepX;
-            Y = NextstepY;	//将NextstepX，NextstepY赋值给x,y，使得下一次循环时使用新的x,y
-            board[X][Y] = StepNum;	//将这一点（x，y）记为此步要走的点
+            Y = NextstepY;	// let (X,Y) equal to the (NextstepX, NextstepY), because we will use the X and y for the next recursive  
+            board[X][Y] = StepNum;	// let the (X, Y) to the Chase board's coordinate and set the current number of movement   
         	}
     	}
     	
@@ -59,21 +60,21 @@ public class Knight_Tour {
     public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int X, Y;
-		//输入初始点的位置
+		//Set the First step 
 		System.out.print("Enter the number of column of the first step: ");
 		X = input.nextInt();
 		System.out.print("Enter the number of row of the first step: ");
 		Y = input.nextInt();
 
         int board[][] = new int[n][n];
-        //将棋盘上的所有点初始化为0
+        // initialize the Chase board to the zero
         for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				board[i][j] = 0;
 			}
 		}
-		board[X][Y] = 1;	//初始点设为第一步
-		//运行程序，输出结果
+		board[X][Y] = 1;	//set the board[X][Y] as the first step   
+		//Output the result whether success or Failure
 		if(Recursion(X, Y, board)) {
 			System.out.println("Success: ");
 			Picture(board);
@@ -82,7 +83,7 @@ public class Knight_Tour {
 		}		
 	}
 	
-	//画出图像
+	//Output the board 
   	static void Picture(int board[][]) { 
           for (int i = 0; i < n; i++) { 
               for (int j = 0; j < n; j++) 
